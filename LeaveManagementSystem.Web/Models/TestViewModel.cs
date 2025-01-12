1178af534c9d987bdf8ace2088c1813d5448eba8
wayne <weijiewang001@gmail.com>
2025-01-12 23:53:36 +1100

    Add project files.

---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth{ get; set; }
+    }
+}
